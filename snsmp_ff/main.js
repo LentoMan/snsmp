45e9312b508119f92cbdec1894ff31a0bc013690
Lennart Johannesson <lentoman@gmail.com>
2019-04-25 22:10:42 +0200

    Add support for new twitter layout & larger images

--- snsmp_ff/main.js
+++ snsmp_ff/main.js
@@ -1,5 +1,26 @@
 'use strict'
 
+var Main = { };
+
+Main.stopPropagation = function(event) {
+  event.stopPropagation();
+  event.preventDefault();
+}
+
+Main.handleTwitterImageOnMatch = function(urlMatches) {
+  if(urlMatches != null && urlMatches.length >= 2) {
+    let url = urlMatches[1];
+
+    let classicUrlMatch = url.match(/(http.*).(?:jpg|gif|png)/i)
+    if(classicUrlMatch != null) {
+      var extension = classicUrlMatch[0].replace(classicUrlMatch[1], "").replace(".","");
+      url = classicUrlMatch[1] + "?format=" + extension + "&name=4096x4096";
+    }
+
+    window.open(url, "_blank")
+  }
+}
+
 document.addEventListener('click', function(event) { 
   if(!OptionsProvider.options.popoutEnabled)
     return;
@@ -7,24 +28,34 @@ document.addEventListener('click', function(event) {
   let t = event.target;
   
   // TweetDeck
-  if(t.classList.contains('js-media-image-link') && !t.parentNode.classList.contains('is-video')) {
-    event.stopPropagation();
-    event.preventDefault();
-    let urlMatches = event.target.style.backgroundImage.match(/(http.*)[\?|\:]/i)
-    if(urlMatches != null && urlMatches.length >= 2) {
-      window.open(urlMatches[1] + ":large", "_blank")
+  
+  if(document.URL.match("(http.*)//tweetdeck.twitter.com") != null) {
+    if(t.classList.contains('js-media-image-link') && !t.parentNode.classList.contains('is-video')) {
+      Main.stopPropagation(event);
+      let urlMatches = event.target.style.backgroundImage.match(/(http.*)[\?|\:]/i)
+      Main.handleTwitterImageOnMatch(urlMatches);
     }
   }
 
-  // Twitter
-  if(t.parentNode.classList.contains('js-adaptive-photo')) {
-    event.stopPropagation();
-    event.preventDefault();
-    let urlMatches = t.src.match(/(http.*)/i)
-    if(urlMatches != null && urlMatches.length >= 2) {
-      window.open(urlMatches[1] + ":large", "_blank")
+  // Twitter (Modern & Classic)
+  if(document.URL.match("(http.*)//twitter.com") != null) {
+    let modernUrlMatch = t.src.match(/(http.*)[\?]format=(?:jpg|gif|png)/i)
+
+    if(t.parentNode.classList.contains('js-adaptive-photo') || modernUrlMatch) {
+      Main.stopPropagation(event);
+
+      let imageUrl = t.src;
+      
+      if(modernUrlMatch != null) {
+        imageUrl = modernUrlMatch[0].replace("?format=",".");
+      }
+
+      let urlMatches = imageUrl.match(/(http.*)/i)
+      Main.handleTwitterImageOnMatch(urlMatches);
     }
   }
+  
+
 }, true);
 
 OptionsProvider.loadOptions();
