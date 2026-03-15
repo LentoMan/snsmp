4421b9a1340b98f289a1c93505a3aeb123236bab
Lennart Johannesson <lentoman@gmail.com>
2026-03-15 22:04:21 +0100

    Reformat javascript

--- snsmp_ff/background.js
+++ snsmp_ff/background.js
@@ -1,37 +1,35 @@
 'use strict'
 
-var BackgroundManager = { };
+var BackgroundManager = {};
 
-BackgroundManager.onError = function(error) {
-    console.error(`Error: ${error}`);
+BackgroundManager.onError = function (error) {
+  console.error(`Error: ${error}`);
 }
 
-BackgroundManager.togglePopoutEnabled = function() {
-    OptionsProvider.options.popoutEnabled = !OptionsProvider.options.popoutEnabled;
-    OptionsProvider.saveOptions();
-    BackgroundManager.updateEnabledIcon();
+BackgroundManager.togglePopoutEnabled = function () {
+  OptionsProvider.options.popoutEnabled = !OptionsProvider.options.popoutEnabled;
+  OptionsProvider.saveOptions();
+  BackgroundManager.updateEnabledIcon();
 
-    browser.tabs
-        .query({})
-        .then(BackgroundManager.broadcastReloadOptionsMessage, BackgroundManager.onError);
+  browser.tabs.query({})
+              .then(BackgroundManager.broadcastReloadOptionsMessage, BackgroundManager.onError);
 }
 
-BackgroundManager.updateEnabledIcon = function() {
-    let iconName = (OptionsProvider.options.popoutEnabled ? `icon-enabled.png` :  `icon-disabled.png`);
-    browser.browserAction.setIcon({ path: iconName });
+BackgroundManager.updateEnabledIcon = function () {
+  let iconName = (OptionsProvider.options.popoutEnabled ? `icon-enabled.png` : `icon-disabled.png`);
+  browser.browserAction.setIcon({ path: iconName });
 }
 
-BackgroundManager.broadcastReloadOptionsMessage = function(tabs) {
-    for (let tab of tabs) {
-        browser.tabs.sendMessage(tab.id, { reloadOptions: true })
-        .then(response => { console.log(response.response); })
-        .catch(BackgroundManager.onError);
-    }
+BackgroundManager.broadcastReloadOptionsMessage = function (tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(tab.id, { reloadOptions: true })
+                .then(response => { console.log(response.response); })
+                .catch(BackgroundManager.onError);
+  }
 }
 
 browser.browserAction.onClicked.addListener(tab => {
-    console.log("Toggle button clicked");
-    BackgroundManager.togglePopoutEnabled();
+  BackgroundManager.togglePopoutEnabled();
 });
 
 OptionsProvider.loadOptions().then(() => { BackgroundManager.updateEnabledIcon() });
